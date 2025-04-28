c4de89af0a3474df8e4893ddbfe89468a744a6fe
Herrera Daniel <danielherrera2009@hotmail.es>
2025-04-27 23:23:21 -0300

    Cambios responsive, flecha responsive, texto

--- js/index.js
+++ js/index.js
@@ -18,13 +18,13 @@ btnLanguage.addEventListener('click', () => {
     if (isSpanish) {
         // nav
         document.getElementById('nav-1').textContent = "Profile";
-        document.getElementById('nav-2').textContent = "Projects";
-        document.getElementById('nav-3').textContent = "Education";
+        document.getElementById('nav-2').textContent = "Education";
+        document.getElementById('nav-3').textContent = "Projects";
         document.getElementById('nav-4').textContent = "Contact";
         document.getElementById('in-language').textContent = "ES";
         // header
         document.getElementById('tag-text').textContent = "¡Looking for new challenges! 🚀";
-        document.getElementById('hi').textContent = "Hello! My name is";
+        document.getElementById('hi').textContent = "Hi! My name is";
         document.getElementById('about').textContent = "I'm a student in the University of Buenos Aires (UTN) Programming Program." +
             " With experience in academic and personal projects using Java, Spring, MySQL, HTML, and JavaScript, " +
             "I'm constantly looking for opportunities to grow, contribute, and continue learning.";
@@ -32,13 +32,20 @@ btnLanguage.addEventListener('click', () => {
             '<i class="fa-solid fa-download"></i>' +
             ' Download CV';
         // fields
-        document.getElementById('title-academic').innerText = "Academic Training";
-        document.getElementById('description-academic').innerText = "Experience through academic paths.";
+        document.getElementById('title-1').innerText = "Education";
+        document.getElementById('description-1').innerText = 
+            "Experience acquired through academic careers.";
+        document.getElementById('title-2').innerText = "Projects";
+        document.getElementById('description-2').innerText = 
+            "Technical challenges that apply the technologies learned.";
+        document.getElementById('title-3').innerText = "Practice";
+        document.getElementById('description-3').innerText = 
+            "Designs made for learning purposes.";
     } else {
         // nav
         document.getElementById('nav-1').textContent = "Perfil";
-        document.getElementById('nav-2').textContent = "Proyectos";
-        document.getElementById('nav-3').textContent = "Educación";
+        document.getElementById('nav-2').textContent = "Educación";
+        document.getElementById('nav-3').textContent = "Proyectos";
         document.getElementById('nav-4').textContent = "Contacto";
         document.getElementById('in-language').textContent = "EN";
         // header
@@ -52,8 +59,15 @@ btnLanguage.addEventListener('click', () => {
             '<i class="fa-solid fa-download"></i>' +
             ' Descargar CV';
         // fields
-        document.getElementById('title-academic').innerText = "Formación Academica";
-        document.getElementById('description-academic').innerText = "Experiencia a través de trayectos de caracter academico.";
+        document.getElementById('title-1').innerText = "Educación";
+        document.getElementById('description-1').innerText = 
+            "Experiencia adquirida mediante trayectos de carácter académico.";
+        document.getElementById('title-2').innerText = "Proyectos";
+        document.getElementById('description-2').innerText = 
+            "Desafíos técnicos que aplican las técnologías aprendidas.";
+        document.getElementById('title-3').innerText = "Prácticas";
+        document.getElementById('description-3').innerText = 
+            "Diseños realizados con fines de aprendizaje.";
     }
     isSpanish = !isSpanish; // cambia de estado
 });
