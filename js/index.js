d56e876bfb2ebb379cdaa48b4a6a4e2ea8773a03
Daniel <danielhazzer2009@gmail.com>
2025-06-26 19:41:26 -0300

    texto

--- js/index.js
+++ js/index.js
@@ -38,10 +38,10 @@ btnLanguage.addEventListener('click', () => {
         document.getElementById('title-2').innerText = "Featured Projects";
         document.getElementById('description-2').innerText = 
             "Technical challenges that apply the technologies learned.";
-        document.getElementById('title-3').innerText = "Practice";
+        document.getElementById('title-3').innerText = "Development Practices";
         document.getElementById('description-3').innerText = 
             "Designs made for learning purposes.";
-        document.getElementById('title-4').innerText = "Contact";
+        document.getElementById('title-4').innerText = "Information and Contact";
         document.getElementById('description-4').innerText = 
             "Any means of contact available.";
     } else {
@@ -68,10 +68,10 @@ btnLanguage.addEventListener('click', () => {
         document.getElementById('title-2').innerText = "Proyectos Destacados";
         document.getElementById('description-2').innerText = 
             "Desafíos técnicos que aplican las técnologías aprendidas.";
-        document.getElementById('title-3').innerText = "Prácticas";
+        document.getElementById('title-3').innerText = "Prácticas y Desarrollo";
         document.getElementById('description-3').innerText = 
             "Diseños realizados con fines de aprendizaje.";
-        document.getElementById('title-4').innerText = "Contacto";
+        document.getElementById('title-4').innerText = "Información y Contacto";
         document.getElementById('description-4').innerText = 
             "Cualquier medio de contacto disponible.";
     }
